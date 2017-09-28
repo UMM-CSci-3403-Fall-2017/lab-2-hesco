1d94988b7e4afad6d8a0f2083346a33303422c58
root <root@DESKTOP-AD8UU62.localdomain>
2017-09-27 23:48:11 -0500

    Added the nessessary code for all test to pass, in the disemvowel code thought about useing a different operator, but for the sake of time just stuck with for and while loops with if statements.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,24 +1,42 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-
 #include "disemvowel.h"
 
-char vowels[11] = {'A','a','E','e','I','i','O','o','U','u', '\0'};
-
 char* disemvowel(char* str) {
-  int m = 0;
-  int 1len;
-  1len = strlen(str);
-  char* home;
-  home = (char*) colloc (1len + 1, sizeof(char));
-
-  while ((c = getchar()) != '\0') {
-	  if(!(isVowel(str[i]))) {
-		  home[m] = str[i];
-		  m++;
-	  }
+  int i=0, j=0, found=0, VC=0, valid=0, len=strlen(str);
+  char* toStr;
+//char lower[6]={'a','e','i','o','u','\0'};
+//char upper[6]={'A','E','I','O','U','\0'};
+  char davaol[11] = {'a','A','e','E','i','I','o','O','u','U','\0'};
+  while(i<len){
+      for(j=0;j<11;j++){
+          if(davaol[j] == str[i]){
+              VC++;
+          }
+      }
+      i++;
   }
-  home[m] = '\0';
-  return home;
+  toStr = (char*) calloc(len - VC + 1,sizeof(char));
+  VC=0;
+for (i=0;i<len;i++){
+    //handel emplty empty cases;
+    found=0;
+    j=0;
+    //strchr(davaol, str[i]) if ture returns pointer, if flase returns NULL, humsdmkadlml
+    // tomuch ochem and not enought time to handle this fancy function.
+    while(j<11){
+        if(davaol[j]==str[i]){
+            found=1;
+            VC++;
+        }
+        j++;    
+    }
+    if(found == 0) {
+        toStr[valid] = str[i];
+        valid++;
+        
+    }
+    
+}
+  return toStr;
 }
